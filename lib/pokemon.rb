d74f796416dccbbd8637652a769aca1d42849126
Christopher Suarez <chrissuarez001@gmail.com>
2023-04-13 19:09:35 -0400

    commit tree 27f5cac57ce803e6222d7ec3d8a90c74651fe1df

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,31 @@
+require 'pry'
 class Pokemon
+    attr_accessor :name, :type, :id, :db
+
+    def initialize(id: , name:, type:, db:)
+    @id, 
+    @name,  
+    @type, 
+    @db = id, name, type
+end
+
+def self.save(name,type, db)
+  
+    db.execute("INSERT INTO pokemon (name, type) VALUES (?,?)", name,type ) 
+    
+  end
+
+def self.find(id,db)
+    
+   character = db.execute("SELECT * FROM pokemon WHERE id = 1")
+   binding.pry 
+   character = self.find
+binding.pry  
+  character.id = ?, 
+  character.name = ?,
+  character.type = ?, 
+character.db =  id, name, type
+  character
+end
+
 end
